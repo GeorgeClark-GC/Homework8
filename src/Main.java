e862e58af8b25d7809105205592440104c395779
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-11 20:01:40 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] array = new int [3];
+        array [0] = 1;
+        array [1] = 2;
+        array [2] = 3;
+        float [] secondArray = {1.57f, 7.654f, 9.986f};
+        short [] thirdArray = {2024, 7, 11, 19, 57};
     }
 }
