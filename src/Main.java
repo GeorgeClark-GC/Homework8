77002eebab055e1621cee52bc8932919e5ee7627
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-11 20:07:26 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,27 @@ public class Main {
         array [2] = 3;
         float [] secondArray = {1.57f, 7.654f, 9.986f};
         short [] thirdArray = {2024, 7, 11, 19, 57};
+        System.out.println("Example2");
+        for (int index = 0; index < array.length; index++) {
+            if (index == array.length - 1) {
+                System.out.println(array[index]);
+                break;
+            }
+            System.out.print(array[index] + ", ");
+        }
+        for (int index = 0; index < secondArray.length; index++) {
+            if (index == secondArray.length - 1) {
+                System.out.println(secondArray[index]);
+                break;
+            }
+            System.out.print(secondArray[index] + ", ");
+        }
+        for (int index = 0; index < thirdArray.length; index++) {
+            if (index == thirdArray.length - 1) {
+                System.out.println(thirdArray[index]);
+                break;
+            }
+            System.out.print(thirdArray[index] + ", ");
+        }
     }
 }
