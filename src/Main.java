d3784e130bc8eb1862a4ee44c33ff9d1a3f19c12
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-12 11:04:05 +0300

    Corrections v1

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int [] array = new int [3];
-        array [0] = 1;
-        array [1] = 2;
-        array [2] = 3;
-        float [] secondArray = {1.57f, 7.654f, 9.986f};
-        short [] thirdArray = {2024, 7, 11, 19, 57};
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        float[] secondArray = {1.57f, 7.654f, 9.986f};
+        short[] thirdArray = {2024, 7, 11, 19, 57};
         System.out.println("Example2");
-        for (int index = 0; index < array.length; index++) {
-            if (index == array.length - 1) {
-                System.out.println(array[index]);
-                break;
-            }
-            System.out.print(array[index] + ", ");
-        }
-        for (int index = 0; index < secondArray.length; index++) {
-            if (index == secondArray.length - 1) {
-                System.out.println(secondArray[index]);
-                break;
-            }
-            System.out.print(secondArray[index] + ", ");
-        }
-        for (int index = 0; index < thirdArray.length; index++) {
-            if (index == thirdArray.length - 1) {
-                System.out.println(thirdArray[index]);
-                break;
-            }
-            System.out.print(thirdArray[index] + ", ");
-        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(secondArray));
+        System.out.println(Arrays.toString(thirdArray));
         System.out.println("Example3");
         int arraySize = array.length;
         for (int index = arraySize - 1; index >= 0; index--) {
@@ -55,14 +39,10 @@ public class Main {
         }
         System.out.println("Example4");
         for (int index = 0; index < array.length; index++) {
-            if (array[index]%2 != 0) {
+            if (array[index] % 2 != 0) {
                 array[index] += 1;
             }
-            if (index == array.length - 1) {
-                System.out.println(array[index]);
-                break;
-            }
-            System.out.print(array[index] + ", ");
         }
+        System.out.println(Arrays.toString(array));
     }
 }
